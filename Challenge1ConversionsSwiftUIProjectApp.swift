4de0abb88da53949392f1706b4bbdf2d4aa438e9
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2024-04-03 13:16:46 +0200

    Add files via upload

---
+++ Challenge1ConversionsSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1ConversionsSwiftUIProjectApp.swift
+//  Challenge1ConversionsSwiftUIProject
+//
+//  Created by Marko Zivanovic on 3.4.24..
+//
+
+import SwiftUI
+
+@main
+struct Challenge1ConversionsSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Challenge1ConversionsSwiftUIProject
+//
+//  Created by Marko Zivanovic on 3.4.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var waterAmount = 0.0
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    
+                }
+            }
+            .navigationTitle("Conversions")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
